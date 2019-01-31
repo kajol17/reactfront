b0c04d5ac8b0eb3486d130bae8f174a69c3d795e
apple <apple@apples-MacBook-Air.local>
2019-01-31 14:35:21 +0530

    Updates

---
+++ precache-manifest.ee6c13fb1586d85abbef673b40dfd359.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c75fc0b94848b387e26c",
+    "url": "/kajol17/reactfront/static/js/runtime~main.c75fc0b9.js"
+  },
+  {
+    "revision": "d47f31ab45cf74e05226",
+    "url": "/kajol17/reactfront/static/js/main.d47f31ab.chunk.js"
+  },
+  {
+    "revision": "9227a8a1d5187c13fb08",
+    "url": "/kajol17/reactfront/static/js/1.9227a8a1.chunk.js"
+  },
+  {
+    "revision": "d47f31ab45cf74e05226",
+    "url": "/kajol17/reactfront/static/css/main.0d2c4e54.chunk.css"
+  },
+  {
+    "revision": "900bc602e6980c313b8ecc9aca3eeaef",
+    "url": "/kajol17/reactfront/index.html"
+  }
+];
